8b59566d37ce39e0c2f671a5397a7f0b2329b378
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-06-10 06:46:28 +0100

    update

--- index.js
+++ index.js
@@ -4,16 +4,8 @@ console.log("TESTING");
 console.log(cats[0].name);
 
 const cardUl = document.querySelector(".cards");
-let catName = cats.name
-let catImg = cats.img
-
-for (let i = 0; i < cats.length; i++) {
-  catName = cats[i].name;
-    catImg = cats[i].image;
-  renderCats(catName, catImg);
-}
-console.log("catIMG", catImg)
-console.log("catname", catName)
+let catName = cats.name;
+let catImg = cats.img;
 
 function getTemp(select) {
   for (let i = 0; i < temperaments.length; i++) {
@@ -27,85 +19,87 @@ function getTemp(select) {
     select.appendChild(option);
   }
 }
-function renderCats(catName, catImg) {
-  const cardLi = document.createElement("li");
-  cardUl.appendChild(cardLi);
-  const button = document.createElement("button");
-  button.textContent = "Delete";
-  const h2 = document.createElement("h2");
-  h2.classList = "card--title";
+function renderCats() {
+  for (let i = 0; i < cats.length; i++) {
+    catName = cats[i].name;
+    catImg = cats[i].image;
 
-  h2.textContent = catName;
-  const img = document.createElement("img");
-  img.width = "256";
-  img.classList = "card--img";
-  img.src = catImg;
-  cardLi.append(button, h2, img);
+    const cardLi = document.createElement("li");
+    cardUl.appendChild(cardLi);
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    const h2 = document.createElement("h2");
+    h2.classList = "card--title";
 
-  const form = document.createElement("form");
-  cardLi.appendChild(form);
-  const formList = document.createElement("ul");
-  formList.classList = "card--text";
-  form.appendChild(formList);
+    h2.textContent = catName;
+    const img = document.createElement("img");
+    img.width = "256";
+    img.classList = "card--img";
+    img.src = catImg;
+    cardLi.append(button, h2, img);
 
-  const ageLi = document.createElement("li");
-  formList.appendChild(ageLi);
-  ageLi.textContent = "Age: ";
-  const ageInput = document.createElement("input");
-  ageInput.name = "age";
-  ageInput.type = "number";
-  ageInput.value = "5";
-  ageLi.appendChild(ageInput);
+    const form = document.createElement("form");
+    cardLi.appendChild(form);
+    const formList = document.createElement("ul");
+    formList.classList = "card--text";
+    form.appendChild(formList);
 
-  const breedLi = document.createElement("li");
-  formList.appendChild(breedLi);
-  breedLi.textContent = "Breed: ";
-  const breedInput = document.createElement("input");
-  breedInput.name = "breed";
-  breedInput.type = "text";
-  breedInput.value = "Tabby";
-  breedLi.appendChild(breedInput);
+    const ageLi = document.createElement("li");
+    formList.appendChild(ageLi);
+    ageLi.textContent = "Age: ";
+    const ageInput = document.createElement("input");
+    ageInput.name = "age";
+    ageInput.type = "number";
+    ageInput.value = "5";
+    ageLi.appendChild(ageInput);
 
-  const colourLi = document.createElement("li");
-  formList.appendChild(colourLi);
-  colourLi.textContent = "Colour: ";
-  const colourInput = document.createElement("input");
-  colourInput.name = "colour";
-  colourInput.type = "number";
-  colourInput.value = "5";
-  colourLi.appendChild(colourInput);
+    const breedLi = document.createElement("li");
+    formList.appendChild(breedLi);
+    breedLi.textContent = "Breed: ";
+    const breedInput = document.createElement("input");
+    breedInput.name = "breed";
+    breedInput.type = "text";
+    breedInput.value = "Tabby";
+    breedLi.appendChild(breedInput);
 
-  const temperamentLi = document.createElement("li");
-  temperamentLi.textContent = "Temperament: ";
-  const select = document.createElement("select");
-  select.name = "temperament";
-  temperamentLi.appendChild(select);
-  formList.append(ageLi, breedLi, colourLi, temperamentLi);
-  getTemp(select);
+    const colourLi = document.createElement("li");
+    formList.appendChild(colourLi);
+    colourLi.textContent = "Colour: ";
+    const colourInput = document.createElement("input");
+    colourInput.name = "colour";
+    colourInput.type = "number";
+    colourInput.value = "5";
+    colourLi.appendChild(colourInput);
 
-  const buttonLi = document.createElement("li");
-  formList.appendChild(buttonLi);
-  const btn = document.createElement("button");
-  btn.type = "submit";
-  btn.textContent = "Update";
-  buttonLi.appendChild(btn);
+    const temperamentLi = document.createElement("li");
+    temperamentLi.textContent = "Temperament: ";
+    const select = document.createElement("select");
+    select.name = "temperament";
+    temperamentLi.appendChild(select);
+    formList.append(ageLi, breedLi, colourLi, temperamentLi);
 
-}
-// renderCats(catName, catImg);
+    getTemp(select);
 
-async function getJoke() {
-    try {
-      const response = await fetch('https://api.chucknorris.io/jokes/random');
-      const joke = await response.json();
-      console.log('async/await version : ' + joke.value);
-    } catch (error) {
-      console.log('error: ', error)
-    }
+    const buttonLi = document.createElement("li");
+    formList.appendChild(buttonLi);
+    const btn = document.createElement("button");
+    btn.type = "submit";
+    btn.textContent = "Update";
+    buttonLi.appendChild(btn);
   }
-
-async function getAllCats() {
-    return new Promise(res => {
-        setTimeout(() => res(cats), 1000)
-    })
 }
-getAllCats()
+
+// select cat list from DOM page
+// define fn fetchCats() that calls getAllCats -> call renderCats() when response comes through
+//     getAllCats is async, use .then to capture result
+// define fn renderCats(catsList) -> renders the list of cats, looping through it
+// define fn renderCat(cat) -> renders elements for 1 cat object
+// call fetchCats()
+
+function fetchCats() {
+  getAllCats().then(renderCats())
+      .catch(function (error) {
+        console.log("error was: " + error);
+      });
+  }
+fetchCats();
