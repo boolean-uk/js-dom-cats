16b59626043a14861debb00af77d4fb85771da2d
Atakan Temizkan <atikotemizkan@gmail.com>
2024-06-13 18:22:48 +0300

    started card

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // console.log(cats);
 // console.log(temperaments);
 
-async function getKitterAll() {
-  const result = await getAllCats();
-  return result;
+const CARDS_UL = document.querySelector(".cards");
+
+function addCardToPage(theCat) {
+  console.log(theCat);
+  const H2 = document.createElement("h2");
+  H2.textContent = theCat.name;
+  CARDS_UL.appendChild(H2);
 }
 
-console.log(getKitterAll());
+async function getKitterAll() {
+  const result = await getAllCats();
+  for (let i = 0; i < result.length; i++) {
+    let theCat = result[i];
+    addCardToPage(theCat);
+  }
+}
+
+getKitterAll();
