8973a7251526eeea9dd353c8547933963e939139
Atakan Temizkan <atikotemizkan@gmail.com>
2024-06-13 17:39:42 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log(cats)
-console.log(temperaments)
+// console.log(cats);
+// console.log(temperaments);
+
+async function getKitterAll() {
+  const result = await getAllCats();
+  return result;
+}
+
+console.log(getKitterAll());
